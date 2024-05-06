75315aa707d30078416584efd87766763bae76f0
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-05-06 18:36:18 +0800

    Added Post Function

--- PetVille/mainPage/views.py
+++ PetVille/mainPage/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html')
+    postedcontent = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'main/home.html', postedcontent)
 
 def about(request):
     return render(request, 'main/about.html', {'title': 'About'})
