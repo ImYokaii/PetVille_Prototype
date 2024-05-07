4c3e04dbda28fcc6e31a4ea4fe09b06c65e3b1cb
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-05-07 13:51:57 +0800

    Added Login and Logout Function

--- PetVille/users/views.py
+++ PetVille/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 from django.contrib import messages
 from .forms import UserRegisterForm 
 # Create your views here.
@@ -11,10 +12,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('mainPage-home')
+            messages.success(request, f'Account Successfully Created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
 
 
