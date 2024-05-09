ae9c96adff1883aa5327097920da42afa8390c8e
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-05-09 23:52:20 +0800

    Added in Update Function

--- PetVille/mainPage/views.py
+++ PetVille/mainPage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 # Create your views here.
 
@@ -22,9 +23,29 @@ class PostDetailView(DetailView):
     #context_object_name = 'posts' For List View
     #ordering = ['-date_posted'] For List View
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post 
+    template_name = 'main/post_form.html'
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post 
+    template_name = 'main/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+    
 def about(request):
     return render(request, 'main/about.html', {'title': 'About'})
