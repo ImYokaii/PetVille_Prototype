b74328cc43c48b61615356c93a3053cdff7aa3fe
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-05-26 19:53:42 +0800

    First Deploy

--- PetVille/PetVille/settings.py
+++ PetVille/PetVille/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n-pyi&kjtx0x5bb&3wc^t3k$ck$nuq(^ql85%u4+d%%rh$xo0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
